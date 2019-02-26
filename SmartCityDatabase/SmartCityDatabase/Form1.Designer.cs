045e7d9fa843331450cbf3ca6072b062c6ff28a4
markom1 <44947815+markom1@users.noreply.github.com>
2019-02-26 22:37:38 +0100

    Database control test

---
+++ SmartCityDatabase/SmartCityDatabase/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace SmartCityDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPosalji = new System.Windows.Forms.Button();
+            this.btnPrikazi = new System.Windows.Forms.Button();
+            this.txtUnos = new System.Windows.Forms.TextBox();
+            this.txtPrikaz = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnPosalji
+            // 
+            this.btnPosalji.Location = new System.Drawing.Point(65, 99);
+            this.btnPosalji.Name = "btnPosalji";
+            this.btnPosalji.Size = new System.Drawing.Size(75, 23);
+            this.btnPosalji.TabIndex = 0;
+            this.btnPosalji.Text = "Posalji";
+            this.btnPosalji.UseVisualStyleBackColor = true;
+            this.btnPosalji.Click += new System.EventHandler(this.btnPosalji_Click);
+            // 
+            // btnPrikazi
+            // 
+            this.btnPrikazi.Location = new System.Drawing.Point(65, 148);
+            this.btnPrikazi.Name = "btnPrikazi";
+            this.btnPrikazi.Size = new System.Drawing.Size(75, 23);
+            this.btnPrikazi.TabIndex = 1;
+            this.btnPrikazi.Text = "Prikazi";
+            this.btnPrikazi.UseVisualStyleBackColor = true;
+            this.btnPrikazi.Click += new System.EventHandler(this.btnPrikazi_Click);
+            // 
+            // txtUnos
+            // 
+            this.txtUnos.Location = new System.Drawing.Point(25, 21);
+            this.txtUnos.Multiline = true;
+            this.txtUnos.Name = "txtUnos";
+            this.txtUnos.Size = new System.Drawing.Size(153, 53);
+            this.txtUnos.TabIndex = 2;
+            // 
+            // txtPrikaz
+            // 
+            this.txtPrikaz.Location = new System.Drawing.Point(25, 195);
+            this.txtPrikaz.Multiline = true;
+            this.txtPrikaz.Name = "txtPrikaz";
+            this.txtPrikaz.Size = new System.Drawing.Size(153, 53);
+            this.txtPrikaz.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(200, 277);
+            this.Controls.Add(this.txtPrikaz);
+            this.Controls.Add(this.txtUnos);
+            this.Controls.Add(this.btnPrikazi);
+            this.Controls.Add(this.btnPosalji);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPosalji;
+        private System.Windows.Forms.Button btnPrikazi;
+        private System.Windows.Forms.TextBox txtUnos;
+        private System.Windows.Forms.TextBox txtPrikaz;
+    }
+}
+
---
+++ SmartCityDatabase/SmartCityDatabase/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace SmartCityDatabase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private MySqlConnection connection;
+        private string server;
+        private string port;
+        private string database;
+        private string uid;
+        private string password;
+
+        public void Initialize()
+        {
+            server = "portmap.io";
+            port = "61974";
+            database = "test";
+            uid = "test";
+            password = "123";
+
+            string connectionString;
+
+            connectionString =
+                "SERVER=" + server + ";" +
+                "PORT=" + port + ";" +
+                "DATABASE=" + database + ";" +
+                "UID=" + uid + ";" +
+                "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        public bool openConnection()
+        {
+            try
+            {
+                this.connection.Open();
+                MessageBox.Show("Uspjesno povezivanje");
+                return true;
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show("Neuspjelo povezivanje.");
+                return false;
+            }
+        }
+
+        public bool closeConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show("Greska");
+                return false;
+            }
+        }
+
+        private void btnPosalji_Click(object sender, EventArgs e)
+        {
+            string insert = "INSERT INTO test VALUES(" + txtUnos.Text + ");";
+            MySqlCommand cmd = new MySqlCommand(insert, connection);
+
+            if (openConnection() == true)
+            {
+                cmd.ExecuteNonQuery();
+            }
+            else MessageBox.Show("Neuspjelo upisivanje podataka.");
+        }
+
+        public List< string >[] Select()
+        {
+            string select = "SELECT * FROM test;";
+
+            List<string>[] list = new List<string>[3];
+            list[0] = new List<string>();
+
+            if (openConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(select, connection);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    list[0].Add(dataReader["unos"] + "");
+                }
+                dataReader.Close();
+
+                closeConnection();
+
+                return list;
+            }
+            else
+            {
+                return list;
+            }
+        }
+
+        private void btnPrikazi_Click(object sender, EventArgs e)
+        {
+            txtPrikaz.Text = Select().ToString();
+        }
+    }
+}
